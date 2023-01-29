74c014263e3a033ca98ecd302c506c1cf674d374
Debjyoti <Debjyoti@Debjyoti-Kittu>
2023-01-29 10:11:45 +0530

    okook

--- firstJavaProject2022/src/com/thread/volatileKeyword/Notes.java
+++ firstJavaProject2022/src/com/thread/volatileKeyword/Notes.java
@@ -5,7 +5,7 @@ public class Notes {
 	//volatile keyword always variable e lagate hoy...
 	//kono variable volatile hole system always setar value ta main memory RAM theke check kore
 	//java cache memory theke chk kore na
-	
+	//okok
 
 	
 	
